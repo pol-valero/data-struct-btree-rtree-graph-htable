23b3577000de34230f4183a63bf4f2c709ba4dcb
Pol Valero <>
2023-02-28 08:16:44 +0100

    Canvi de l'estructura de la classe MSTPrim

--- src/Algorithms/MSTprim.java
+++ src/Algorithms/MSTprim.java
@@ -4,22 +4,38 @@ import Entities.Graph;
 import Entities.KnownRoute;
 import Entities.PlaceOfInterest;
 
+import java.util.ArrayList;
+
 public class MSTprim {
 
     public static void frequentRoutesDetection(Graph graph) {
 
-        KnownRoute[][] mst = new KnownRoute[graph.getSize()][graph.getSize()];
         PlaceOfInterest origin = graph.getFirstNode();
-        mstPrim(mst, origin);
-        printMST(mst);
+        ArrayList<PlaceOfInterest> mstNodes = new ArrayList<>();
+        ArrayList<KnownRoute> mstRoutes = new ArrayList<>();
+
+        mstPrim(graph, origin, mstNodes, mstRoutes);
+        printMST(mstNodes, mstRoutes);
 
     }
 
-    private static void mstPrim (KnownRoute[][] mst, PlaceOfInterest origin) {
+    private static void mstPrim (Graph graph, PlaceOfInterest origin, ArrayList<PlaceOfInterest> mstNodes, ArrayList<KnownRoute> mstRoutes) {
+
+        mstNodes.add(origin);
+
+        while (mstNodes.size() < graph.getSize()) {
+
+            KnownRoute minRoute = null;
+            PlaceOfInterest[] nodes = graph.getPlaces();
+
+            //minRoute = findMinAdjacentRoute
+
+            mstRoutes.add(minRoute);
+        }
 
     }
 
-    private static void printMST (KnownRoute[][] mst) {
+    private static void printMST (ArrayList<PlaceOfInterest> mstNodes, ArrayList<KnownRoute> mstRoutes) {
 
     }
 
