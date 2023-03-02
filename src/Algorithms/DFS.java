b02b62dfe6bf69c72c78e26336e7eb7e6ced18fb
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2023-03-02 22:56:36 +0100

    Classe Queue canviada (arreglada) i afegit l'algorisme DFS combinat amb el BFS.

---
+++ src/Algorithms/DFS.java
@@ -0,0 +1,23 @@
+package Algorithms;
+
+import Entities.Graph;
+import Entities.PlaceOfInterest;
+
+public class DFS {
+
+	public static void kingdomExploration(Graph graph, PlaceOfInterest initialNode) {
+		// Avoid being shown the same node twice (in case it was called from BFS algorithm since it was already visited).
+		if (!initialNode.isVisited()) {
+			initialNode.justVisited();
+			System.out.println(initialNode.showInformation());
+		}
+
+		PlaceOfInterest[] adjacents = graph.getAdjacents(initialNode);
+		for (PlaceOfInterest adjacent : adjacents) {
+			// Check if adjacent node belongs to the same kingdom.
+			if (!adjacent.isVisited() && adjacent.sameKingdom(initialNode)) {
+				kingdomExploration(graph, adjacent);
+			}
+		}
+	}
+}
