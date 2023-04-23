5ddecd645a9c20f7eb8d13c0b0b6dc2cad9def8c
Pol Valero <pol.valero.arjona@gmail.com>
2023-04-23 20:14:09 +0200

    Classe rectangle creada

---
+++ src/RTreesF3/Entities/Rectangle.java
@@ -0,0 +1,15 @@
+package RTreesF3.Entities;
+
+public class Rectangle {
+
+    public Point maxPoint;  //Top right corner
+    public Point minPoint;  //Bottom left corner
+    public Point centerPoint;  //Center point, used to measure distance between rectangles
+
+    public Rectangle(Point maxPoint, Point minPoint) {
+        this.maxPoint = maxPoint;
+        this.minPoint = minPoint;
+        this.centerPoint = new Point((maxPoint.x + minPoint.x) / 2, (maxPoint.y + minPoint.y) / 2);
+    }
+
+}
